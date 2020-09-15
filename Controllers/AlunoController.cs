e2e5d626565525a153f071e91d17793a8be1bbed
JBMILHOUSE <43553799+JBMILHOUSE@users.noreply.github.com>
2020-09-15 19:52:52 -0300

    first commit

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoEscola_API.Data;
+using ProjetoEscola_API.Models;
+using System.Linq;
+
+namespace ProjetoEscola_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : Controller 
+    {
+        private readonly EscolaContext _context;
+        public AlunoController(EscolaContext context)
+        {
+           //constructor
+           _context = context;
+
+        }
+        
+        [HttpGet]
+        public ActionResult<List<Aluno>> GetAll() 
+        {
+           return _context.Aluno.ToList();
+        }
+    }
+    
+
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,12 @@
+namespace ProjetoEscola_API.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string RA { get; set; }
+
+        public string Nome { get; set; }
+
+        public int CodCurso { get; set; }
+    }
+}
