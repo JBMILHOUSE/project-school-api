4cf43b3480917e6ef96d88f37eeeafdee06a9e4b
JBMILHOUSE <43553799+JBMILHOUSE@users.noreply.github.com>
2020-09-25 14:03:10 -0300

    validação final

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -15,7 +15,7 @@ namespace ProjetoEscola_API.Models
         public string nome { get; set; }
 
         [Required(ErrorMessage = "Campo obrigatório")]
-     //   [Range(2, 0,  ErrorMessage = "Este campo deve conter no máximo 2 caracteres.")]
+        [Range(0, 99,  ErrorMessage = "Este campo deve conter no máximo 2 caracteres.")]
         public int codCurso { get; set; }
     }
 }
